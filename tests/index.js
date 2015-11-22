2ccb7fa0a2bea42f7acb7e6ed79c8fa403be1f1d
Gregor Martynus <gregor@martynus.net>
2015-11-22 00:46:09 -0500

    test: temporarily disable browsers
    https://github.com/hoodiehq/hoodie-zuul-config/issues/1

--- tests/index.js
+++ tests/index.js
@@ -23,37 +23,42 @@ test('zuul config values', function (t) {
 })
 
 test('zuul browsers', function (t) {
-  t.is(zuulConfig.browsers.length, 7, 'settings for 7 browsers')
+  t.is(zuulConfig.browsers.length, 1, 'settings for 7 browsers')
 
   var chrome = browserSettingFor(zuulConfig, 'chrome')
-  t.is(Object.keys(chrome).length, 2, '2 settings for browsers.chrome')
-  t.is(chrome.version, '-1..latest', 'Test in latest two versions')
-  t.is(chrome.platform, 'Windows 10', 'Test in Windows 10')
+  t.is(Object.keys(chrome).length, 1, '1 setting for browsers.chrome')
+  t.is(chrome.version, 'latest', 'Test in latest two versions')
 
-  var firefox = browserSettingFor(zuulConfig, 'firefox')
-  t.is(Object.keys(firefox).length, 2, '2 settings for browsers.firefox')
-  t.is(firefox.version, '-1..latest', 'Test in latest two versions')
-  t.is(firefox.platform, 'Windows 10', 'Test in Windows 10')
-
-  var edge = browserSettingFor(zuulConfig, 'microsoftedge')
-  t.is(Object.keys(edge).length, 1, '1 setting for browsers.microsoftedge')
-  t.is(edge.version, 'latest', 'Test in latest version')
-
-  var ie = browserSettingFor(zuulConfig, 'internet explorer')
-  t.is(Object.keys(ie).length, 1, '1 setting for browsers["internet explorer"]')
-  t.is(ie.version, '11', 'Test in Version 11')
-
-  var safari = browserSettingFor(zuulConfig, 'safari')
-  t.is(Object.keys(safari).length, 1, '1 setting for browsers.safari')
-  t.is(safari.version, '-2..latest', 'Test in latest three versions')
-
-  var iPhone = browserSettingFor(zuulConfig, 'iphone')
-  t.is(Object.keys(iPhone).length, 1, '1 setting for browsers.iphone')
-  t.is(iPhone.version, '-2..latest', 'Test in latest three versions')
-
-  var android = browserSettingFor(zuulConfig, 'android')
-  t.is(Object.keys(android).length, 1, '1 setting for browsers.android')
-  t.deepEqual(android.version, ['4.4', '-1..latest'], 'Test in 4.4 and latest two versions')
+  // TODO: https://github.com/hoodiehq/hoodie-zuul-config/issues/1
+  // var chrome = browserSettingFor(zuulConfig, 'chrome')
+  // t.is(Object.keys(chrome).length, 2, '2 settings for browsers.chrome')
+  // t.is(chrome.version, '-1..latest', 'Test in latest two versions')
+  // t.is(chrome.platform, 'Windows 10', 'Test in Windows 10')
+  //
+  // var firefox = browserSettingFor(zuulConfig, 'firefox')
+  // t.is(Object.keys(firefox).length, 2, '2 settings for browsers.firefox')
+  // t.is(firefox.version, '-1..latest', 'Test in latest two versions')
+  // t.is(firefox.platform, 'Windows 10', 'Test in Windows 10')
+  //
+  // var edge = browserSettingFor(zuulConfig, 'microsoftedge')
+  // t.is(Object.keys(edge).length, 1, '1 setting for browsers.microsoftedge')
+  // t.is(edge.version, 'latest', 'Test in latest version')
+  //
+  // var ie = browserSettingFor(zuulConfig, 'internet explorer')
+  // t.is(Object.keys(ie).length, 1, '1 setting for browsers["internet explorer"]')
+  // t.is(ie.version, '11', 'Test in Version 11')
+  //
+  // var safari = browserSettingFor(zuulConfig, 'safari')
+  // t.is(Object.keys(safari).length, 1, '1 setting for browsers.safari')
+  // t.is(safari.version, '-2..latest', 'Test in latest three versions')
+  //
+  // var iPhone = browserSettingFor(zuulConfig, 'iphone')
+  // t.is(Object.keys(iPhone).length, 1, '1 setting for browsers.iphone')
+  // t.is(iPhone.version, '-2..latest', 'Test in latest three versions')
+  //
+  // var android = browserSettingFor(zuulConfig, 'android')
+  // t.is(Object.keys(android).length, 1, '1 setting for browsers.android')
+  // t.deepEqual(android.version, ['4.4', '-1..latest'], 'Test in 4.4 and latest two versions')
 
   t.end()
 })
